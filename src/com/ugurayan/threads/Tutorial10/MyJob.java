b1b8c03359d327c98eec0873807a5e4c5ba57e72
Ugur Ayan <ugur.ayan@gmail.com>
2017-11-11 14:35:38 +0100

    Updated Wait and Notify for Producer and Consumer Pattern

---
+++ src/com/ugurayan/threads/Tutorial10/MyJob.java
@@ -0,0 +1,36 @@
+package com.ugurayan.threads.Tutorial10;
+
+
+import java.util.Scanner;
+
+public class MyJob {
+    public void producer() throws InterruptedException {
+
+        synchronized (this){ // this means myJob class
+
+            System.out.println("Producer thread running ...");
+            wait();  // All object has a wait method
+            System.out.println("Resumed producer ...");
+
+        }
+
+    }
+
+    public void consumer() throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        Thread.sleep(2000);
+
+        synchronized (this){ // this means myJob class
+
+            System.out.println("Waiting for return key...");
+
+            scanner.nextLine();
+
+            System.out.println("Return key pressed...");
+            notify(); // only used in syncronized code block like wait
+
+            Thread.sleep(5000);
+        }
+    }
+}
---
+++ src/com/ugurayan/threads/Tutorial10/Tutorial10.java
@@ -0,0 +1,45 @@
+package com.ugurayan.threads.Tutorial10;
+
+/**-----------------------------------------------------------------------------------------------------------
+ * Subject : Wait and notify
+ *
+ * Created by: Ugur Ayan
+ * Date: 11/11/2017.
+ ------------------------------------------------------------------------------------------------------------*/
+
+
+public class Tutorial10 {
+    public static void main(String[] args) throws InterruptedException {
+
+        MyJob m = new MyJob();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    m.producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    m.consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+    }
+}
