2c9e335d8f087762c6204fd2717c228396a2852c
Ugur Ayan <ugur.ayan@gmail.com>
2017-11-12 21:43:47 +0100

    Tutorial 13 totally added

--- src/com/ugurayan/threads/Tutorial13/Tutorial13.java
+++ src/com/ugurayan/threads/Tutorial13/Tutorial13.java
@@ -6,47 +6,28 @@ package com.ugurayan.threads.Tutorial13;
  */
 public class Tutorial13 {
 
-    public static final Object lock1 = new Object();
-    public static final Object lock2 = new Object();
-    private int index;
+    public static void main(String[] args) throws Exception {
+        final Runner runner = new Runner();
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.firstThread();
+                } catch (InterruptedException ignored) {}
+            }
+        });
 
-    public static void main(String[] a) {
-        Thread t1 = new Thread1();
-        Thread t2 = new Thread2();
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.secondThread();
+                } catch (InterruptedException ignored) {}
+            }
+        });
+
         t1.start();
         t2.start();
-    }
-
-    private static class Thread1 extends Thread {
-
-        public void run() {
-            synchronized (lock1) {
-                System.out.println("Thread 1: Holding lock 1...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ignored) {}
-                System.out.println("Thread 1: Waiting for lock 2...");
-                synchronized (lock2) {
-                    System.out.println("Thread 2: Holding lock 1 & 2...");
-                }
-            }
-        }
-    }
-
-    private static class Thread2 extends Thread {
-
-        public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread 2: Holding lock 2...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ignored) {}
-                System.out.println("Thread 2: Waiting for lock 1...");
-                synchronized (lock1) {
-                    System.out.println("Thread 2: Holding lock 2 & 1...");
-                }
-            }
-        }
+        t1.join();
+        t2.join();
+        runner.finished();
     }
 }
-
