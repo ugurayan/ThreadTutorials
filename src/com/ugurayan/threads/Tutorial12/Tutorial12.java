2bc137700aedb3030e49db1f9c3eefcb19879fb8
Ugur Ayan <ugur.ayan@gmail.com>
2017-11-12 20:11:17 +0100

    Tutorial 12

--- src/com/ugurayan/threads/Tutorial12/Tutorial12.java
+++ src/com/ugurayan/threads/Tutorial12/Tutorial12.java
@@ -1,7 +1,35 @@
 package com.ugurayan.threads.Tutorial12;
 
-/**
- * Created by future on 11/12/17.
+/*
+Re entrant locks
  */
 public class Tutorial12 {
+
+    public static void main(String[] args) throws Exception {
+        final Runner runner = new Runner();
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.firstThread();
+                } catch (InterruptedException ignored) {
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    runner.secondThread();
+                } catch (InterruptedException ignored) {
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        runner.finished();
+    }
+
 }
