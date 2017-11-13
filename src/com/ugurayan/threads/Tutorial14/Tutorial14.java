5c279e209917ebf4d45ad745fcca62dc37d4fa3b
Ugur Ayan <ugur.ayan@gmail.com>
2017-11-13 23:09:32 +0100

    updated Semaphore Main app

--- src/com/ugurayan/threads/Tutorial14/Tutorial14.java
+++ src/com/ugurayan/threads/Tutorial14/Tutorial14.java
@@ -1,13 +1,33 @@
-/**-----------------------------------------------------------------------------------------------------------
+/**
+ * -----------------------------------------------------------------------------------------------------------
  * Subject : Semaphores Threads
- *
+ * <p>
  * Created by: Ugur Ayan
  * Date: 11/13/2017.
- ------------------------------------------------------------------------------------------------------------*/
+ * ------------------------------------------------------------------------------------------------------------
+ */
 
 package com.ugurayan.threads.Tutorial14;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class Tutorial14 {
 
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 20; i++) { //200 hundred times will be called
+            executor.submit(new Runnable() {
+                public void run() {
+                    Connectionn.getInstance().connect();
+                }
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
 }
